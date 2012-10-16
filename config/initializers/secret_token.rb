e532285c73b28e4b59ebecf14e6e1e9827d7ee76
Chris Power <cpower@teladoc.com>
2012-10-16 16:51:09 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fantasyhaus::Application.config.secret_token = '3770ffe8c0bfb8866d49958810b0528d4bfa35feb817b9c0e8101dae3c09e4fcd8a6c74c0dbfcde2003005e625fb26e050942dcc99c550b311defecfd59c9841'
