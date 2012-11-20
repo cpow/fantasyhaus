0990c60480a406e15742b86490bbd95721eab950
cpow <cpow85@gmail.com>
2012-11-20 17:58:36 -0500

    after_initialize happens well before after_save

--- app/models/token_expiration_observer.rb
+++ app/models/token_expiration_observer.rb
@@ -2,6 +2,6 @@ class TokenExpirationObserver < Mongoid::Observer
   observe :yahoo_auth
 
   def after_initialize(record)
-    record.refresh_access_token if record.expired?
+    record.refresh_access_token if record.access_token_expiration and record.expired?
   end
 end
